9c916d8007a129e148bd8ac1f898e8dcd8936bcc
Fatima <fatimashraf0098@gmail.com>
2023-06-28 18:30:57 +0500

    online db

--- backend/db.js
+++ backend/db.js
@@ -1,6 +1,6 @@
 const mongoose=require('mongoose')
-const mongoURL='mongodb://localhost:27017/LaptopStore'
-
+// const mongoURL='mongodb://localhost:27017/LaptopStore'
+const mongoURL='mongodb+srv://fatimaashraf0098:znag7sXHFB1ppBeo@cluster0.26pgriw.mongodb.net/?retryWrites=true&w=majority'
 const connectToMong=()=>{
     mongoose.connect(mongoURL,()=>{
         console.log("conncted to mongo")
